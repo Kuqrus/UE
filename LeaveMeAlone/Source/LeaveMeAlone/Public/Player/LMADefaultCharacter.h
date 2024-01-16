a83c9058a8ce6ac7cd5478d728edd4ec9c3429c5
Kuqrus <kuqrus@gmail.com>
2024-01-16 19:47:14 +0500

    Zoom added

--- LeaveMeAlone/Source/LeaveMeAlone/Public/Player/LMADefaultCharacter.h
+++ LeaveMeAlone/Source/LeaveMeAlone/Public/Player/LMADefaultCharacter.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "LMADefaultCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
+
 UCLASS()
 class LEAVEMEALONE_API ALMADefaultCharacter : public ACharacter
 {
@@ -19,11 +23,47 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY()
+	UDecalComponent* CurrentCursor = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	UMaterialInterface* CursorMaterial = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	FVector CursorSize = FVector(20.f, 40.0f, 40.0f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	uint8 ZoomSensitivity = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	int MinZoom = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	int MaxZoom = 5600;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	float YRotation = -75.0f;
+	float ArmLength = 1400.0f;
+	float FOV = 55.0f;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void ZoomIn();
+	void ZoomOut();
+	void ZoomReset();
 
 };
